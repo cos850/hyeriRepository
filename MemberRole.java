95716fdd99488239683206ca0fc660d483893602
cos850 <68280248+cos850@users.noreply.github.com>
2021-03-19 20:16:30 +0900

    Add files via upload

---
+++ MemberRole.java
@@ -0,0 +1,10 @@
+package com.dung.geul.entity;
+
+public enum MemberRole {
+    USER, // 기본 회원
+    ADMIN, // 관리자
+    STUDENT, // 학생
+    ENTERPRISER, //기업
+    MENTO, // 멘토
+    COUNSELOR   // 상담사
+}
